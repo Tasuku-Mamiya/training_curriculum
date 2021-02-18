51e79705bb483143ff06ea552e43b568e3a0c99e
Tasuku-Mamiya <tasukusao26@gmail.com>
2021-02-18 16:45:33 +0900

    issue4_2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
@@ -36,7 +36,7 @@ class CalendarsController < ApplicationController
       end
 
       wdays_num = 
-      if wdays<= 7
+      if wdays <= 7
         wday_num = wday_num - 7
       end
 
